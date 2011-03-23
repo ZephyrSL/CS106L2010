c6efb1b38c49197fa3a48a8268c4a4847b9fd572
Stephen Li <stephen.liziyun@gmail.com>
2011-03-23 23:19:03 +0800

    X macros -- Some bugs./test ./test

---
+++ color.h
@@ -0,0 +1,17 @@
+/* Lecture code 4.2
+ *
+ * color.h file for sample 4.1.  The macros contained in here
+ * are of the format DEFINE_COLOR(color, opposite) and can be
+ * used by a calling application to export this information as
+ * needed.  See the second half of Handout #08 for more
+ * information.
+ */
+ 
+DEFINE_COLOR(Red, Cyan)
+DEFINE_COLOR(Green, Magenta)
+DEFINE_COLOR(Blue, Yellow)
+DEFINE_COLOR(Cyan, Red)
+DEFINE_COLOR(Magenta, Green)
+DEFINE_COLOR(Yellow, Blue)
+DEFINE_COLOR(White, Black)
+DEFINE_COLOR(Black, White)
